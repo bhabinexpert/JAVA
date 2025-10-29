86f401d534c0cf933fea47005ada0023637c8c50
Bhabin Dulal <bhabindulal35@gmail.com>
2025-10-29 20:22:13 +0545

    variables

---
+++ Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        //Variables:
+
+        int a = 23;
+        int b = 45;
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
