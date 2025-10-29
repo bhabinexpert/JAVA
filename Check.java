d87af8663f8223c90e064b52699914dc87ab3577
Bhabin Dulal <bhabindulal35@gmail.com>
2025-10-29 20:33:18 +0545

    Add Input class to read and print user input

---
+++ Check.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Check {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int x = sc.nextInt();
+//
+//        if(x%2==0){
+//            System.out.println("Even");
+//        }
+//        else {
+//            System.out.println("Odd");
+//        }
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if( a == b){
+            System.out.println("Equal");
+        } else{
+            if(a > b){
+                System.out.println("a is greater than b");
+            }
+            else{
+                System.out.println("b is greater than a");
+            }
+        }
+    }
+}
---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Input {
+    public static void main(String[] args) {
+        //Input
+
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next(); // takes only one word/token with next();
+        // if wants to print the whole sentences as an output then use nextln();
+        System.out.println(name);
+    }
+}
