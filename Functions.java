5d5f2277223d0602987da7ea90d1c402a45ff858
Bhabin Dulal <bhabindulal35@gmail.com>
2025-10-31 19:59:21 +0545

    Functions defined and solved some basics

--- Functions.java
+++ Functions.java
@@ -6,11 +6,26 @@ public class Functions {
         System.out.println(name);
         
     }
+    public static int calculateSum(int a, int b){
+        int sum = a+b;
+        return sum;
+    }
+
+    public static int calculateProduct(int a, int b){
+        return a*b;
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.next();
+//        String name = sc.next();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
 
-        printMyName(name);
+        int sum = calculateSum(a, b);
+        System.out.println("The sum of two numbers is " + sum);
+
+        int product = calculateProduct(a, b);
+        System.out.println("The product of two numbers is " + product);
+//        printMyName(name);
     }
     
 }
