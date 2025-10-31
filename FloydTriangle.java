917b89c29c7866496f1a3cbfef7b7e824188d360
Bhabin Dulal <bhabindulal35@gmail.com>
2025-10-31 09:07:04 +0545

    Solved floyd's Triangle print pattern

---
+++ FloydTriangle.java
@@ -0,0 +1,14 @@
+public class FloydTriangle {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 1;
+        for(int i = 1; i<=a; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(b);
+                b++;
+            }
+            System.out.println();
+        }
+    }
+    
+}
