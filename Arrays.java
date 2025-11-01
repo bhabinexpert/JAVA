f30dfe6b89109e1db7b5e9241146c58fe28f44d3
Bhabin Dulal <bhabindulal35@gmail.com>
2025-11-01 21:13:18 +0545

    Arrays practise

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+public class Arrays {
+    public static void main(String[] args) {
+        int[] marks = new int[4];
+        marks[0] = 30;
+        marks[1]= 50;
+        marks[2]= 80;
+        marks[3]= 100;
+
+
+//        System.out.println("Marks are:" + marks[0] + " " + marks[1] + " " + marks[2] + " " + marks[3]);
+
+        for(int i =0; i<=3; i++){
+            System.out.println("Marks is: " + marks[i]);
+        }
+    }
+}
