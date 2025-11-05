a644321ce921721a96d583b9bbcacbd26196302d
Bhabin Dulal <bhabindulal35@gmail.com>
2025-11-05 19:28:31 +0545

    Strings Started

---
+++ Strings.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine(); // Inputs full Sentences.
+         System.out.println("Your name is " + name);
+    }
+}
