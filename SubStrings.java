bec2e0a77b923421ad819269ba7b6de1fbf085a2
Bhabin Dulal <bhabindulal35@gmail.com>
2025-11-05 19:41:47 +0545

    Substrings

---
+++ SubStrings.java
@@ -0,0 +1,8 @@
+public class SubStrings {
+    public static void main(String[] args) {
+        String Sentence = "I am Vabin.";
+
+        String Name = Sentence.substring(5);
+        System.out.print(Name);
+    }
+}
