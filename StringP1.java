cdb44d2680e054fd8088812302c638952b23faf1
Bhabin Dulal <bhabindulal35@gmail.com>
2025-11-05 19:53:12 +0545

    Problem 1 of string solved

---
+++ StringP1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class StringP1 {
+    public static void main(String[] args) {
+       // Take an array of Strings input from the user & find the cumulative (combined) length of all those strings.
+       Scanner input = new Scanner(System.in);
+       int size = input.nextInt();
+
+       String array[] = new String[size];
+       int totalLength = 0;
+
+       for (int i =0; i<size; i++){
+           array[i] = input.next();
+           totalLength += array[i].length();
+       }
+       System.out.println(totalLength);
+    }
+}
