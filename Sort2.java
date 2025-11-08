0eb367d8a7f136f610c116c79a9dda452b4c6a3e
Bhabin Dulal <bhabindulal35@gmail.com>
2025-11-08 11:59:28 +0545

    Insertion sort

---
+++ Sort2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Sort2 {
+    //insertion sort:
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+    }
+    public static void main(String args[]) {
+        int []arr = {4, 1, 5, 2, 3};
+
+
+        for (int i =1; i<arr.length; i++){
+            int current = arr[i];
+            int j = i-1;
+            while(j>=0 && current < arr[j]){
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j+1] = current;
+        }
+        printArray(arr);
+    }
+}
