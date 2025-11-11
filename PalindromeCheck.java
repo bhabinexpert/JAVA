ad04d0363c2219112b0bb04a8f0b2de45bbca7b8
Bhabin Dulal <bhabindulal35@gmail.com>
2025-11-11 21:22:31 +0545

    Palindrome checking

--- PalindromeCheck.java
+++ PalindromeCheck.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class PalindromeCheck {
+    public static boolean isPalindrome(String str, int start, int end) {
+        if (start >= end) return true;
+        if (str.charAt(start) != str.charAt(end)) return false;
+        return isPalindrome(str, start + 1, end - 1);
+    }
+
+    public static void main(String[] args) {
+        String str = "madam";
+        System.out.println(isPalindrome(str, 0, str.length() - 1));
+    }
 }
